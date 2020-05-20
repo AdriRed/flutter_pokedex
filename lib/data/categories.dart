ab2dfb32ea1327fb61378b0fedab04dff438675a
Adri Rojo <adriamilmarcos@gmail.com>
2020-05-20 16:51:18 +0200

    Addition of custom page

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -4,5 +4,6 @@ import '../models/category.dart';
 const List<Category> categories = [
   Category(name: "Pokedex", color: AppColors.teal, route: "/pokedex-api"),
   Category(name: "Favourites", color: AppColors.blue, route: "/favourites"),
-  Category(name: "Custom", color: AppColors.lightRed, route: "/custom"),
+  Category(
+      name: "Custom\npokemon", color: AppColors.lightRed, route: "/customs"),
 ];
---
+++ lib/screens/customs/customs.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomsPage extends StatefulWidget {
+  CustomsPage({Key key}) : super(key: key);
+
+  @override
+  _CustomsPageState createState() => _CustomsPageState();
+}
+
+class _CustomsPageState extends State<CustomsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Custom"),
+    );
+  }
+}
