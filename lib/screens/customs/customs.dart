2b23d1f07f179f551b73c49b9abe4f0aaf528847
Adri Rojo <adriamilmarcos@gmail.com>
2020-05-21 17:52:13 +0200

    customs delete working

--- lib/screens/customs/customs.dart
+++ lib/screens/customs/customs.dart
@@ -105,20 +105,65 @@ class _CustomsPageState extends State<CustomsPage>
             ),
             padding: EdgeInsets.only(left: 28, right: 28, bottom: 58),
             itemCount: list.length,
-            itemBuilder: (context, index) => CustomsApiCard(
-              index: list[index].id,
-              image: base64Decode(list[index].photo),
-              name: list[index].name,
-              type1: pokeapiModel.pokemonTypes
-                  .firstWhere((element) => element.info.id == list[index].type1)
-                  .info,
-              type2: pokeapiModel.pokemonTypes
-                  .firstWhere((element) => element.info.id == list[index].type2)
-                  .info,
-              // onPress: () {
-              //   sessionModel.setFavouritesIndex(index);
-              //   Navigator.of(context).pushNamed("/favourites-info");
-              // },
+            itemBuilder: (context, index) => GestureDetector(
+              onLongPress: () => {
+                showDialog(
+                  context: context,
+                  child: AlertDialog(
+                    title:
+                        Text("Do you want to delete " + list[index].name + "?"),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          "Nope",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text(
+                          "A la brossa",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        },
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  if (value) {
+                    this.setState(() {
+                      _loading = true;
+                    });
+                    PokemonHelper.removeCustom(list[index].id, (favourite) {
+                      sessionModel.removeCustom(list[index].id);
+                      this.setState(() {
+                        _loading = false;
+                      });
+                    }, (body) => log(body));
+                  }
+                })
+              },
+              child: CustomsApiCard(
+                index: list[index].id,
+                image: base64Decode(list[index].photo),
+                name: list[index].name,
+                type1: pokeapiModel.pokemonTypes
+                    .firstWhere(
+                        (element) => element.info.id == list[index].type1)
+                    .info,
+                type2: pokeapiModel.pokemonTypes
+                    .firstWhere(
+                        (element) => element.info.id == list[index].type2)
+                    .info,
+                // onPress: () {
+                //   sessionModel.setFavouritesIndex(index);
+                //   Navigator.of(context).pushNamed("/favourites-info");
+                // },
+              ),
             ),
           ),
         );
